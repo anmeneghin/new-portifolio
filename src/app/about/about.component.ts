d4f9416c063f89e1d2f38b502c0a8c15b9d7b771
Amanda Nogueira Meneghin <amanda.meneghin@hdrup.com>
2024-01-31 23:11:12 -0300

    skills e about

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,12 +6,40 @@ import { RouterLink } from '@angular/router';
   selector: 'app-about',
   standalone: true,
   imports: [CommonModule, RouterLink],
-  template: ` <a
-      class="semi-transparent-button btn-left"
-      [routerLink]="['/skills']"
+  template: `
+    <a class="semi-transparent-button btn-left" [routerLink]="['/skills']"
       ><i class="fa-solid fa-angles-left"></i> Skills
     </a>
-    <p>about works!</p>`,
+    <div class="content">
+      <h1 class="mb-4 titulo">About</h1>
+      <div class="card">
+        <div class="card-body">
+          <p class="about-open">&lt;about&gt;</p>
+          <p class="sobre">
+            Desenvolvedora Front-end há 4 anos. Tenho experiência em Angular e
+            suas aplicações em single-spa, sendo para desevolver novos projetos
+            ou melhorar ainda mais o que foi desenvolvido, pois estou em
+            constante aprendizado. Gosto de trabalhar com estilização de
+            aplicações e aprecio buscar novidades sobre web desing. Tenho
+            experiencia em PHP, refatorando aplicações para otimizar o
+            desempenho de frontend.
+          </p>
+          <p class="about-close">&lt;/about&gt;</p>
+          <hr />
+          <div class="info">
+            <b>Idade: </b> 25 anos
+            <br />
+            <b>Email: </b>
+            <a class="email" href="mailto:anm3219@gmail.com"
+              ><font>anm3219&#64;gmail.com</font></a
+            >
+            <br />
+            <b>Currículo: </b> <a><i class="fa-solid fa-download"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  `,
   styleUrl: './about.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
--- src/app/animation.ts
+++ src/app/animation.ts
@@ -9,7 +9,7 @@ import {
 } from '@angular/animations';
 
 export const slideInAnimation = trigger('routeAnimations', [
-  transition('HomePage <=> SkillsPage', [
+  transition('HomePage => SkillsPage, SkillsPage => AboutPage', [
     style({ position: 'relative', top: '50%' }),
     query(':enter, :leave', [
       style({
@@ -30,32 +30,25 @@ export const slideInAnimation = trigger('routeAnimations', [
       }),
     ]),
   ]),
-  transition('* <=> *', [
-    style({ position: 'relative' }),
-    query(
-      ':enter, :leave',
-      [
-        style({
-          position: 'absolute',
-          top: '50%',
-          left: 0,
-          width: '100%',
-        }),
-      ],
-      { optional: true }
-    ),
-    query(':enter', [style({ right: '-100%' })], { optional: true }),
+  transition('SkillsPage => HomePage, AboutPage => SkillsPage', [
+    style({ position: 'relative', top: '50%' }),
+    query(':enter, :leave', [
+      style({
+        position: 'absolute',
+        top: '50%',
+        left: 0,
+        width: '100%',
+      }),
+    ]),
+    query(':enter', [style({ left: '-100%' })], { optional: true }),
     query(':leave', animateChild(), { optional: true }),
     group([
-      query(
-        ':leave',
-        [animate('700ms ease-out', style({ right: '100%', opacity: 0 }))],
-        { optional: true }
-      ),
-      query(':enter', [animate('800ms ease-out', style({ right: '0%' }))], {
+      query(':leave', [animate('800ms ease-out', style({ left: '100%' }))], {
         optional: true,
       }),
-      query('@*', animateChild(), { optional: true }),
+      query(':enter', [animate('800ms ease-out', style({ left: '0%' }))], {
+        optional: true,
+      }),
     ]),
   ]),
 ]);
