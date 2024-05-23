32ef34a1cdc24ef18cf2b57083c3b2b9d002e307
Amanda Nogueira Meneghin <amanda.meneghin@hdrup.com>
2024-05-23 20:16:57 -0300

    Auto-generated commit

--- chunk-6FBRJ64E.js
+++
@@ -1,1 +0,0 @@
-import{l as a}from"./chunk-LOU66DEN.js";import"./chunk-F34XZ22Y.js";export{a as AboutComponent};
---
+++ chunk-RUWU6HJJ.js
@@ -0,0 +1,1 @@
+import{l as a}from"./chunk-V7ONK6TP.js";import"./chunk-F34XZ22Y.js";export{a as AboutComponent};
